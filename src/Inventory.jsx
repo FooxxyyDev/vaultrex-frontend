a9d1427cda7a680685d991a38c6e812ff8b41ece
FooxxyyDev <glader666@gmail.com>
2025-09-20 08:58:40 +0200

    Update Inventory.jsx

--- src/Inventory.jsx
+++ src/Inventory.jsx
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+export default function Inventory() {
+  const [scanResult, setScanResult] = useState("");
+
+  return (
+    <div>
+      <h2>Inventory</h2>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) setScanResult(result?.text);
+          if (!!error) console.error(error);
+        }}
+        constraints={{ facingMode: "environment" }}
+        style={{ width: "100%" }}
+      />
+      <p>Senast skannade: {scanResult}</p>
+    </div>
+  );
+}
 
